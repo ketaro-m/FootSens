42b2b9ba0bcfc3385fe661f5c4ead67b71a992d8
Keitaro Murakami <keitaro.mrkm@gmail.com>
2019-10-29 22:04:27 +0900

    add this function

---
+++ loadMap.m
@@ -0,0 +1,11 @@
+function Z = loadMap(X,Y,weight,position,sigma)
+if length(weight)~=size(position,1)
+    msg = 'Invalid input size.';
+    error(msg)
+end
+tmp = gaussC(X,Y,sigma,position(1,:));
+for i=2:length(weight)
+    tmp = cat(3,tmp,gaussC(X,Y,sigma,position(i,:)));
+end
+tmp = tmp.*reshape(weight,[1,1,2]);
+Z = sum(tmp,3);
